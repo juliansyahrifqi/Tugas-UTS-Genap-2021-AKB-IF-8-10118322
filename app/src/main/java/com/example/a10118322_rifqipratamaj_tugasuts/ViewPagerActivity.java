eff03b1235584d943eeb02f0dc10f2c7caa4ac84
juliansyahrifqi <rifqipratama9@gmail.com>
2021-06-05 11:36:54 +0700

    finished viewpager

--- app/src/main/java/com/example/a10118322_rifqipratamaj_tugasuts/ViewPagerActivity.java
+++ app/src/main/java/com/example/a10118322_rifqipratamaj_tugasuts/ViewPagerActivity.java
@@ -1,9 +1,17 @@
 package com.example.a10118322_rifqipratamaj_tugasuts;
 
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.core.content.ContextCompat;
 import androidx.viewpager2.widget.ViewPager2;
 
+import android.content.Intent;
+import android.media.Image;
 import android.os.Bundle;
+import android.view.View;
+import android.view.ViewGroup;
+import android.widget.Button;
+import android.widget.ImageView;
+import android.widget.LinearLayout;
 
 import java.util.ArrayList;
 import java.util.List;
@@ -11,16 +19,44 @@ import java.util.List;
 public class ViewPagerActivity extends AppCompatActivity {
 
     private ViewPagerAdapter viewPagerAdapter;
+    private LinearLayout layoutViewPagerIndicators;
+    private Button buttonGetStarted;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.view_page_slider);
 
+        layoutViewPagerIndicators = findViewById(R.id.viewpager_indicators);
+        buttonGetStarted = findViewById(R.id.button_get_started);
+
         setupViewPagerItems();
 
         ViewPager2 viewPager = findViewById(R.id.viewpager);
         viewPager.setAdapter(viewPagerAdapter);
+
+        setupViewPagerIndicators();
+        setCurrentViewPagerIndicator(0);
+
+        viewPager.registerOnPageChangeCallback(new ViewPager2.OnPageChangeCallback() {
+            @Override
+            public void onPageSelected(int position) {
+                super.onPageSelected(position);
+                setCurrentViewPagerIndicator(position);
+            }
+        });
+
+        buttonGetStarted.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                if(viewPager.getCurrentItem() + 1 < viewPagerAdapter.getItemCount()) {
+                    viewPager.setCurrentItem(viewPager.getCurrentItem() + 1);
+                } else {
+                    startActivity(new Intent(getApplicationContext(), MainActivity.class));
+                    finish();
+                }
+            }
+        });
     }
 
     private void setupViewPagerItems() {
@@ -46,6 +82,49 @@ public class ViewPagerActivity extends AppCompatActivity {
         viewPagerItems.add(thirdSlider);
 
         viewPagerAdapter = new ViewPagerAdapter(viewPagerItems);
+    }
 
+    private void setupViewPagerIndicators() {
+        ImageView[] indicators = new ImageView[viewPagerAdapter.getItemCount()];
+
+        LinearLayout.LayoutParams layoutParams = new LinearLayout.LayoutParams(
+                ViewGroup.LayoutParams.WRAP_CONTENT, ViewGroup.LayoutParams.WRAP_CONTENT
+        );
+
+        layoutParams.setMargins(8, 0, 8, 0);
+
+        for(int i = 0; i < indicators.length; i++) {
+            indicators[i] = new ImageView(getApplicationContext());
+            indicators[i].setImageDrawable(ContextCompat.getDrawable(
+                    getApplicationContext(),
+                    R.drawable.unselected
+            ));
+            indicators[i].setLayoutParams(layoutParams);
+            layoutViewPagerIndicators.addView(indicators[i]);
+        }
+    }
+
+    private void setCurrentViewPagerIndicator(int index) {
+        int childCount = layoutViewPagerIndicators.getChildCount();
+
+        for(int i = 0; i < childCount; i++) {
+            ImageView imageView = (ImageView) layoutViewPagerIndicators.getChildAt(i);
+
+            if(i == index) {
+                imageView.setImageDrawable(
+                        ContextCompat.getDrawable(getApplicationContext(), R.drawable.selected)
+                );
+            } else {
+                imageView.setImageDrawable(
+                        ContextCompat.getDrawable(getApplicationContext(), R.drawable.unselected)
+                );
+            }
+        }
+
+        if(index == viewPagerAdapter.getItemCount() - 1) {
+            buttonGetStarted.setText("GET STARTED");
+        } else {
+            buttonGetStarted.setText("Next");
+        }
     }
 }
